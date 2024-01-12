5d97be3707f39ede9a7b44ad4c8e7fac6e9d3ff4
lx111lx <46951903+lx111lx@users.noreply.github.com>
2024-01-12 17:19:55 +0800

    Update weread.py

--- scripts/weread.py
+++ scripts/weread.py
@@ -37,7 +37,7 @@ WEREAD_CHAPTER_INFO = "https://i.weread.qq.com/book/chapterInfos"
 WEREAD_READ_INFO_URL = "https://i.weread.qq.com/book/readinfo"
 WEREAD_REVIEW_LIST_URL = "https://i.weread.qq.com/review/list"
 WEREAD_BOOK_INFO = "https://i.weread.qq.com/book/info"
-BOOKMARK_ICON_URL = "https://www.notion.so/icons/bookmark_gray.svg"
+BOOKMARK_ICON_URL = None
 
 """这段Pro里没有"""
 def parse_cookie_string(cookie_string):
