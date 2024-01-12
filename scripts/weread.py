96f81e40b1008d5b351c5264b630bcf612c56f8b
lx111lx <46951903+lx111lx@users.noreply.github.com>
2024-01-12 15:46:02 +0800

    Update weread.py Lx
    错误：
    File "/home/runner/work/weread2notion/weread2notion/scripts/weread.py", line 221, in insert_to_notion
        response = client.pages.create(parent=parent, icon=icon, properties=properties)
    NameError: name 'icon' is not defined

--- scripts/weread.py
+++ scripts/weread.py
@@ -211,11 +211,12 @@ def insert_to_notion(bookName, bookId, cover, sort, author, isbn, rating, catego
             properties["Synopsis"] = get_rich_text(read_info.get("bookInfo").get("intro"))
         """notion_helper.get_date_relation(properties,finishedDate)#最后这个像是把阅读结束日期的值传给notion_helper库，不是很清楚，先注释掉"""
 
-    #if cover.startswith("http"):
-    #    icon = get_icon(cover)
-    # 原版没有下面这个else，加一下看是什么
-    """else:
+    """if cover.startswith("http"):
+        icon = get_icon(cover)
+    原版没有下面这个else，加一下看是什么
+    else:
         icon = get_icon(BOOKMARK_ICON_URL)"""
+    icon = get_icon(BOOKMARK_ICON_URL)
     # notion api 限制100个block
     """原版代码"""
     response = client.pages.create(parent=parent, icon=icon, properties=properties)
