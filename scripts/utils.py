fa7c084009f193f5904a63d47e7e28c39489ff8f
lx111lx <46951903+lx111lx@users.noreply.github.com>
2024-01-13 00:17:40 +0800

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -99,7 +99,7 @@ def get_quote(content):
     }
 
 
-def get_callout(content, style, colorStyle, reviewId):
+"""def get_callout(content, style, colorStyle, reviewId):
     # 根据不同的划线样式设置不同的emoji 直线type=0 背景颜色是1 波浪线是2
     emoji = "〰️"
     if style == 0:
@@ -123,6 +123,44 @@ def get_callout(content, style, colorStyle, reviewId):
         color = "yellow"
     return {
             "type": "callout",
+            "callout": {
+                "rich_text": [
+                    {
+                        "type": "text",
+                        "text": {
+                            "content": content[:MAX_LENGTH],
+                        },
+                    }
+                ],
+                "icon": {"emoji": emoji},
+                "color": color,
+            },
+        }"""
+
+def get_callout(content, style, colorStyle, reviewId):
+    # 根据不同的划线样式设置不同的emoji 直线type=0 背景颜色是1 波浪线是2
+    color = "brown_background"
+    if style == 0:
+        color = "green_background"
+    elif style == 1:
+        color = "purple_background"
+    # 如果reviewId不是空说明是笔记
+    if reviewId != None:
+        emoji = "➰"
+    color = "default"
+    # 根据划线颜色设置文字的颜色
+    if colorStyle == 1:
+        color = "red"
+    elif colorStyle == 2:
+        color = "purple"
+    elif colorStyle == 3:
+        color = "blue"
+    elif colorStyle == 4:
+        color = "green"
+    elif colorStyle == 5:
+        color = "yellow"
+    return {
+            "type": "rich_text",
             "callout": {
                 "rich_text": [
                     {
