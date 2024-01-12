40bdef95fbb3aa4a21297a7d8ed0c49e6f24cd32
lx111lx <46951903+lx111lx@users.noreply.github.com>
2024-01-12 16:03:01 +0800

    Update weread.py Lx

--- scripts/weread.py
+++ scripts/weread.py
@@ -135,7 +135,7 @@ def insert_to_notion(bookName, bookId, cover, sort, author, isbn, rating, catego
         "ISBN": get_rich_text(isbn),
         "Resource": get_url(f"https://weread.qq.com/web/reader/{calculate_book_str_id(bookId)}"),
         #"Author": get_rich_text(author),
-        "Author": get_select([author]),
+        "Author": get_select(author.list()),
         "Sort": get_number(sort),
         #这行Pro里没有，"Rating": get_number(rating),注释掉
         "Cover": get_file(cover),
